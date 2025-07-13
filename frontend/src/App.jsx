0c8fd1437e20fd385e8866fffcdf9efffade7b86
Okamissu <Okamissu@gmail.com>
2025-07-13 20:08:04 +0200

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,28 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Work in progress...</h1>
+      <div
+        class="tenor-gif-embed"
+        data-postid="21519082"
+        data-share-method="host"
+        data-aspect-ratio="1.09589"
+        data-width="100%"
+      >
+        <a href="https://tenor.com/view/bleach-urahara-kuske-gif-21519082">
+          Bleach Urahara GIF
+        </a>
+        from <a href="https://tenor.com/search/bleach-gifs">Bleach GIFs</a>
+      </div>{' '}
+      <script
+        type="text/javascript"
+        async
+        src="https://tenor.com/embed.js"
+      ></script>
+    </>
+  )
+}
+
+export default App
