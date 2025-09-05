c75df05f6b2b0149ce5ab80675972c455c92e784
Okamissu <Okamissu@gmail.com>
2025-09-05 14:38:09 +0200

    feat: Update base URL for API and routing to reflect server deployment path

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,5 @@ import tailwind from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwind()],
+  base: '/~20_kobylarz/qa-app/', // Set the correct base path for your app
 })
